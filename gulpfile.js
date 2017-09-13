fbce932a139006e0b6a1cd30e3ba09b98322245e
Bill Donovan <williamcdonovan@gmail.com>
2017-09-12 18:08:28 -0600

    started gulp task runner for testing

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+	plugz = require('gulp-load-plugins'),
+	gutil = require('gulp-util');
+
+	gulp.task('watch', () => {
+
+		return gulp.src('')
+				.pipe()
+				.(gulp.dest())
+	})
